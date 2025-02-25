82cfa6ead6edec459a76c85b752de9e4b2a3a4fc
Stella Wing <stellawing@ucsb.edu>
2025-02-24 20:03:17 -0800

    Stella's shiny app edits 2/24 8:03 pm

--- app.R
+++ app.R
@@ -35,6 +35,7 @@ custom_css <- "
 "
 
 # Create the user interface (this is the front end side of the Shiny App)
+# Modify the UI to include an option for displaying all species or a specific species
 ui <- fluidPage(
   
   # Apply the cerulean theme using shinythemes and add custom CSS
@@ -54,39 +55,39 @@ ui <- fluidPage(
     ),
     tabPanel("Whale Sightings Trends and Seasonality",  # Second tab (was first)
              sidebarLayout(
-               sidebarPanel("Put my widgets here", 
-                            radioButtons(
-                              inputId = "whale_species", 
-                              label = "Choose whale species", 
-                              choices = c("Humpback Whale", "Fin Whale", "Blue Whale") 
-                            ), 
-                            selectInput(inputId = "pt_color",  # Changed from inputID to inputId
-                                        label = "Select Point Color", 
-                                        choices = c("Roses are red" = "red",
-                                                    "Violets are purple" = "purple",
-                                                    "Oranges are ..." = "orange"))), 
-               mainPanel("Put my graph here", 
-                         plotOutput(outputId = "whale_plot"), 
-                         h3("Summary Table"), 
-                         tableOutput(outputId = "penguin_table"))  # Changed outputID to outputId
+               sidebarPanel(
+                 radioButtons(
+                   inputId = "whale_species", 
+                   label = "Choose whale species", 
+                   choices = c("Humpback Whale", "Fin Whale", "Blue Whale", "All Species")  # Add "All Species" option
+                 ), 
+                 selectInput(inputId = "pt_color", 
+                             label = "Select Point Color", 
+                             choices = c("Roses are red" = "red",
+                                         "Violets are purple" = "purple",
+                                         "Oranges are ..." = "orange"))
+               ), 
+               mainPanel(
+                 plotOutput(outputId = "whale_plot"), 
+                 h3("Summary Table"), 
+                 tableOutput(outputId = "penguin_table")
+               )
              )
     ), 
-    tabPanel("Whale Migration Forecast and Time Series Analysis",  # Moved tab before Resources
+    tabPanel("Whale Migration Forecast and Time Series Analysis", 
              h3("Advanced Exploratory Data Analysis"),
              p("This section will contain advanced exploratory data analysis features. Currently, it is under development.")
     ),
-    tabPanel("Additional Resources",  # Resources tab now moved after Advanced EDA
+    tabPanel("Additional Resources", 
              h4("For more information on reducing whale strikes, check out the following document:"),
              a("Reduce Whale Strikes - Top 5 Things You Should Know", 
                href = "https://media.fisheries.noaa.gov/dam-migration/reduce-whale-strikes-top5things.pdf", 
                target = "_blank"), 
-             # Link to the second URL
              p("Learn more about whales on the Ocean.org website:"),
              a("Whales - Ocean.org", 
                href = "https://ocean.org/whales/", 
                target = "_blank"), 
              br(),
-             # Link to the third PDF
              p("For more information, check out this Whale Alert Smartphone App flyer:"),
              a("Whale Alert Smartphone App Flyer", 
                href = "https://media.fisheries.noaa.gov/dam-migration/whale-alert-smartphone-app-flier.pdf", 
@@ -98,13 +99,15 @@ ui <- fluidPage(
 )
 
 # Create the server function 
-# Create the server function 
 server <- function(input, output) {
   
   # Reactive expression for the filtered whale data based on the selected species
   whale_select <- reactive({
-    whale_relevant |> 
-      filter(species == input$whale_species)  # Filter data by the selected species
+    if (input$whale_species == "All Species") {
+      return(whale_relevant)  # Return all species data
+    } else {
+      return(whale_relevant |> filter(species == input$whale_species))  # Filter by selected species
+    }
   })
   
   # Render the plot based on the filtered data
@@ -118,7 +121,6 @@ server <- function(input, output) {
            y = "Number of Sightings") +
       scale_x_continuous(breaks = seq(min(whale_select()$year), max(whale_select()$year), by = 1))  # Show every year on the x-axis
   })
-  
   
   # Example reactive table (if required)
   penguin_sum_table <- reactive({
